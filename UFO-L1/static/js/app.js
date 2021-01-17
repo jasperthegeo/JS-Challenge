8c2a0b1ff134598ace37104814d24354e3dc9633
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-01-16 19:35:59 -0500

    Read the table from data.js

--- UFO-L1/static/js/app.js
+++ UFO-L1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
+var button = = d3.select("#filter-btn");
 
-// YOUR CODE HERE!
+//Read the table from data.js
+var tbody = d3.select("tbody");
+
+    tableData.forEach(function(entry){
+        var tableRow = tbody.append('tr');
+        tableRow.append('td').text(entry.datetime);
+        tableRow.append('td').text(entry.city);
+        tableRow.append('td').text(entry.state);
+        tableRow.append('td').text(entry.country);
+        tableRow.append('td').text(entry.shape);
+        tableRow.append('td').text(entry.durationMinutes);
+        tableRow.append('td').text(entry.comments);
+        });
+
